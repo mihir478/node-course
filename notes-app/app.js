c4d490c88c456a3580f9c33245d062cf37141401
Mihir478 <mihir.h.sanghavi@gmail.com>
2020-06-22 14:07:24 -0400

    Add file apis

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,3 @@
-const fs = require('fs')
+const name = require('./utils.js')
 
-fs.writeFileSync('notes.txt', 'This file was created by Node.js!')
-
-fs.appendFileSync('notes.txt', '\nWatch netflix and then chill!')
+console.log(name)
